dad2342de21f767d3d2c068b6cbbb6d91a5634eb
Ellis McDougald <ecmcdougald@gmail.com>
2025-07-03 20:49:11 -0600

    Plot route on map

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,28 @@ import (
 )
 
 func main() {
+	/*
+	var query string = `
+	[out:json][timeout:30];
+
+	// Find Canada area by ISO3166 code (country)
+	area["ISO3166-1"="CA"][admin_level=2]->.canada;
+
+	// Find Edmonton area inside Canada (admin_level=6)
+	area["name"="Edmonton"][admin_level=6](area.canada)->.edmonton_area;
+
+	// Query ways inside Edmonton area
+	(
+		way["highway"]["area"!~"yes"]["highway"!~"motorway|motorway_link|raceway|construction|service"]["bicycle"!~"no"](area.edmonton_area);
+		way["highway"="cycleway"]["bicycle"!~"no"](area.edmonton_area);
+	);
+	out body;
+	>;
+	out skel qt;
+	`
+	data.GetOSMData(query)
+	*/
+
 	fileName := filepath.Join("osm_bike_data.json")
 	network, _ := data.BuildGraph(fileName)
 
@@ -25,6 +47,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
-
-	
 }
--- web/js/map.js
+++ web/js/map.js
@@ -1,7 +1,27 @@
-// Leaflet map -- centred on Edmonton
+// Map centred on Edmonton
 const map = L.map("map").setView([53.5461, -113.4938], 12);
+
 // Add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
+
+// Plot route on map
+document.body.addEventListener("htmx:afterOnLoad", (event) => {
+  if (event.detail.target.id === "route-data") {
+    console.log("1");
+    try {
+      const geojson = JSON.parse(event.detail.xhr.responseText);
+
+      L.geoJSON(geojson, {
+        style: {
+          color: "blue",
+          weight: 5,
+        },
+      }).addTo(map);
+    } catch (err) {
+      console.error("Error parsing or displaying GeoJSON:", err);
+    }
+  }
+});
