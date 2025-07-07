d6f28b3ee225862adec97bf0da790b9edf1979d3
Ellis McDougald <ecmcdougald@gmail.com>
2025-07-07 15:17:45 -0600

    Fetch geojson data from static file

--- web/js/map.js
+++ web/js/map.js
@@ -1,11 +1,44 @@
-// Map centred on Edmonton
-const map = L.map("map").setView([53.5461, -113.4938], 12);
+// Map centred on Edmonton and bounded on Edmonton
+const edmontonBounds = [
+  [53.3951, -113.7167],
+  [53.7169, -113.2437],
+];
+
+const map = L.map("map", {
+  maxBounds: edmontonBounds,
+  maxBoundsViscosity: 1.0,
+  minZoom: 10,
+}).setView([53.5461, -113.4938], 12);
 
 // Add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
+  minZoom: 10,
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
+
+fetch("edmonton_bike_data_geo.json")
+  .then((response) => {
+    if (!response.ok) throw new Error("No response from file.");
+    return response.json();
+  })
+  .then((geojson) => {
+    L.geoJSON(geojson, {
+      style: {
+        color: "red",
+        weight: 1,
+        opacity: 0,
+      },
+      interactive: true,
+      onEachFeature: (feature, layer) => {
+        let name = feature.properties.name || "Unnamed street";
+        layer.bindPopup(`<strong>${name}</strong>`);
+      },
+    }).addTo(map);
+  })
+  .catch((error) => {
+    console.error("Could not load GeoJSON file.");
+  });
 
 // Plot route on map
 document.body.addEventListener("htmx:afterOnLoad", (event) => {
@@ -18,6 +51,11 @@ document.body.addEventListener("htmx:afterOnLoad", (event) => {
         style: {
           color: "blue",
           weight: 5,
+        },
+        interactive: true,
+        onEachFeature: (feature, layer) => {
+          let name = feature.properties.name || "Unnamed street";
+          layer.bindPopup(`<strong>${name}</strong>`);
         },
       }).addTo(map);
     } catch (err) {
