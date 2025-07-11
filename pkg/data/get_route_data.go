a8ea92ff4c5a80e0655ed858d6d65967ade7811a
Ellis McDougald <ecmcdougald@gmail.com>
2025-07-11 15:23:24 -0600

    Improves error handling in renamed get_route_data.go

---
+++ pkg/data/get_route_data.go
@@ -0,0 +1,77 @@
+package data
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/ellismcdougald/edmonton-bike-map/pkg/model"
+)
+
+func getGeometryLine(coordinateStr string) []model.Coordinate {
+	coordinateStr = strings.TrimPrefix(coordinateStr, "MULTILINESTRING ")
+	coordinateStr = strings.Trim(coordinateStr, "()")
+	coordinatePairs := strings.Split(coordinateStr, ",")
+
+	var allCoordinates []model.Coordinate
+	for _, pair := range coordinatePairs {
+		pair = strings.TrimSpace(pair)
+		coords := strings.Split(pair, " ")
+		latitude, _ := strconv.ParseFloat(coords[0], 64)
+		longitude, _ := strconv.ParseFloat(coords[1], 64)
+
+		allCoordinates = append(allCoordinates, model.Coordinate{
+			Latitude: latitude,
+			Longitude: longitude,
+		})
+	}
+
+	return allCoordinates
+}
+
+func GetRouteData(fileName string) []model.Route {
+	rawData := readCsv(fileName)
+
+	var routeData []model.Route
+	for i, values := range rawData {
+		//var allCoords []model.Coordinate
+		fmt.Println(values[9])
+		getGeometryLine(values[9])
+		/*
+		for j, raw_coords := range values[9] {
+			coords := model.Coordinate {
+				Latitude: raw_coords[0],
+				Longitude: raw_coords[1],
+			}
+			allCoords[j] = coords
+		}
+
+		route := model.Route {
+			Id: values[0],
+			Duration: values[4],
+			GeometryLine: allCoords,
+		}
+		routeData[i] = route
+		*/
+		if i == 1 { break }
+	}
+
+	return routeData
+}
+
+func readCsv(fileName string) [][]string {
+	f, _ := os.Open(fileName)
+	defer func() {
+		if cerr := f.Close(); cerr != nil {
+			log.Printf("Error closing file %s in readCsv: %v", fileName, cerr)
+		}
+	}()
+
+	csvReader := csv.NewReader(f)
+	records, _ := csvReader.ReadAll()
+
+	return records
+}
