a56a45ff7398d2d7c720e32d6d007f183b1632ef
Ellis McDougald <ecmcdougald@gmail.com>
2025-07-18 19:09:17 -0600

    Fix TestComputeWayWeightBikeLane after tweaks to bike friendliness multiplier

--- pkg/data/build_graph_test.go
+++ pkg/data/build_graph_test.go
@@ -82,7 +82,7 @@ func TestComputeWayWeightBikeLane(t *testing.T) {
 		"motor_vehicle": "no",
 	}
 
-	wantWeight := distance * 0.9 * 0.9 * 0.9 * 0.9
+	wantWeight := distance * 0.9 * 0.9 * 0.95
 	gotWeight := computeWayWeight(distance, tags)
 
 	if math.Abs(gotWeight-wantWeight) > 1e-6 {
