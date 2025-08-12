75aeb5a2a97cce30739c6dd999a6a7d33e442786
Ellis McDougald <ecmcdougald@gmail.com>
2025-08-12 13:43:14 -0600

    Update comments

--- pkg/routing/interface.go
+++ pkg/routing/interface.go
@@ -2,6 +2,8 @@ package routing
 
 import "github.com/ellismcdougald/edmonton-bike-map/pkg/model"
 
+// Router defines route-finding functionality.
+// It finds the shortest route between start and end coordinates in a network.
 type Router interface {
 	FindRouteFromCoordinates(network *model.Graph, startLatitude, startLongitude, endLatitude, endLongitude float64) (dist float64, path []int64)
 }
