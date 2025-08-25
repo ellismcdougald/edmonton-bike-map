2fef44aefd511ad8eab4db99e6904e8621559802
Ellis McDougald <ecmcdougald@gmail.com>
2025-08-25 11:39:31 -0600

    Add logout functionality

--- frontend/src/lib/components/MenuBar.svelte
+++ frontend/src/lib/components/MenuBar.svelte
@@ -20,14 +20,20 @@
 -->
 
 <script lang="ts">
-	import { getUsernameFromToken } from '$lib/utils/auth';
+	import { getUsernameFromToken, removeToken } from '$lib/utils/auth';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	let username: string | null = null;
 
 	onMount(() => {
 		username = getUsernameFromToken();
 	});
+
+	function logout() {
+		removeToken();
+		goto('/login');
+	}
 </script>
 
 <div class="flex items-center justify-between px-4 py-2 w-full">
@@ -35,11 +41,27 @@
 	<div class="w-1/3">
 		<h1 class="text-3xl font-bold text-center">Edmonton Bike Map</h1>
 	</div>
-	<div class="w-1/3 text-right">
-		{#if username}
-			<h2>{username}</h2>
-		{:else}
-			<a href="/login" class="text-blue-500 underline hover:text-blue-700"> Log In </a>
-		{/if}
+	<div class="w-1/3 flex justify-end">
+		<div class="relative inline-block group">
+			<button
+				id="usernameButton"
+				class="text-black text-center px-4 py-1 hover:underline focus:outline-none"
+			>
+				{username}
+			</button>
+			<div
+				class="absolute left-0 mt-2 w-full bg-white text-gray-800 rounded shadow-lg z-50
+             opacity-0 invisible translate-y-1 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0
+             transition-all duration-200 ease-out"
+			>
+				<button
+					id="logoutButton"
+					class="block w-full px-4 py-2 text-center hover:bg-gray-100"
+					on:click={logout}
+				>
+					Log out
+				</button>
+			</div>
+		</div>
 	</div>
 </div>
