277469fb848e79f2ff6106508e54f8c391bb1d20
Ellis McDougald <ecmcdougald@gmail.com>
2025-11-13 21:57:21 -0700

    fix: remove unused dburl variable

--- frontend/e2e/lookUpRoute.test.ts
+++ frontend/e2e/lookUpRoute.test.ts
@@ -19,8 +19,6 @@ test.describe('route finding with a test user', () => {
 
 	let client: Client;
 	test.beforeAll(async () => {
-		const dbUrl = `postgres://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}?sslmode=disable`;
-
 		client = new Client(dbConfig);
 		await client.connect();
 
