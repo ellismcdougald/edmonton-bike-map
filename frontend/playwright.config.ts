9f78fe075af7edf2a5e28904f27aaad198033ffc
Ellis McDougald <ecmcdougald@gmail.com>
2025-11-13 22:44:20 -0700

    test: revert to using webserver for playwright

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	workers: 1,
+	webServer: {
+		command: 'npm run build && npm run preview',
+		port: 4173
+	},
 	testDir: 'e2e'
 });
