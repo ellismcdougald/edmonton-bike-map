b4de05a57fd47ec36b8b4a844c97ca0c466563aa
Ellis McDougald <ecmcdougald@gmail.com>
2025-12-07 17:59:01 -0700

    feat(map): redirect to login if token not set

--- frontend/src/routes/map/+page.server.ts
+++ frontend/src/routes/map/+page.server.ts
@@ -1,6 +1,7 @@
 // +page.server.ts
 import type { PageServerLoad } from './$types';
 import { getUsernameFromToken } from '$lib/utils/auth';
+import { redirect } from '@sveltejs/kit';
 
 const API_URL = process.env.API_URL;
 const ALL_WAYS_ENDPOINT = `${API_URL}/api/all-ways`;
@@ -8,6 +9,9 @@ const ALL_WAYS_ENDPOINT = `${API_URL}/api/all-ways`;
 export const load: PageServerLoad = async ({ fetch, cookies }) => {
 	// Decode username
 	const token: string | undefined = cookies.get('session');
+	if (!token) {
+		throw redirect(302, '/login');
+	}
 	const username: string | null = token ? getUsernameFromToken(token) : null;
 
 	// Get ways
