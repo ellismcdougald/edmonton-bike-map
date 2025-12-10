e2713ddb11e169dbea4b98d1d5ff6b100c6aa6e4
Ellis McDougald <ecmcdougald@gmail.com>
2025-12-10 13:34:43 -0700

    fix: only add authorization header to requests to API_URL

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,4 +1,5 @@
 import type { Handle, HandleFetch } from '@sveltejs/kit';
+import { API_URL } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const token = event.cookies.get('session');
@@ -8,7 +9,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	const token = event.locals.token;
-	if (token) {
+	if (token && request.url.startsWith(API_URL)) {
 		request.headers.set('Authorization', `Bearer ${token}`);
 	}
 	return fetch(request);
