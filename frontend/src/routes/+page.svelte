aab7548ebd1a739a91d2c0ddee93b85f48482490
Ellis McDougald <ecmcdougald@gmail.com>
2025-12-09 19:55:29 -0700

    feat(landing-page): remove page at / and add redirect to /map

--- frontend/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-	import Display from '$lib/components/Display.svelte';
-	import MenuBar from '$lib/components/MenuBar.svelte';
-</script>
-
-<MenuBar />
-<Display ways={null} />
